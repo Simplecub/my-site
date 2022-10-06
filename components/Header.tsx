0669a7ba75b7fa931f54cd562b6386871c006dae
Simplecub <exdimail@gmail.com>
2022-10-06 12:16:18 +0400

    main

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+import Navbar from "./Navbar";
+import {FC} from "react";
+
+const Header:FC = () => (
+    <header>
+        <Navbar/>
+    </header>
+
+)
+
+export default Header
